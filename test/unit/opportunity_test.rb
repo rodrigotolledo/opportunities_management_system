aeb442135a1351eb31d1de7f483bd2d3d97ebabb
Rodrigo Tolledo <rodrigotolledo@gmail.com>
2013-05-28 18:28:33 -0300

    adding unit test coverage for Opportunity model.

--- test/unit/opportunity_test.rb
+++ test/unit/opportunity_test.rb
@@ -1,7 +1,57 @@
 require 'test_helper'
 
 class OpportunityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+ # fixtures :Oportunity
+
+ test "opportunity is saved successfully" do	
+ 	@opportunity_unitTest = Opportunity.new :code => '001',
+ 	:company => 'Company 001',
+ 	:address => 'Address 001',
+ 	:salary => '1000',
+ 	:start_date => '04-08-2013', 
+ 	:end_date	=> '04-08-2014',
+ 	:requirements => 'Computers'
+ 	assert @opportunity_unitTest.save
+ end
+
+ test "opportunity is copied successfully" do
+ 	@opportunity_unitTest = Opportunity.new :code => '001',
+ 	:company => 'Company 001',
+ 	:address => 'Address 001',
+ 	:salary => '1000',
+ 	:start_date => '04-08-2013', 
+ 	:end_date	=> '04-08-2014',
+ 	:requirements => 'Computers'
+ 	assert @opportunity_unitTest.save
+ 	opportunity_unitTest_copy = Opportunity.find(@opportunity_unitTest.id)
+ 	assert_equal @opportunity_unitTest, opportunity_unitTest_copy
+ end
+
+ test "opportunity is updated successfully" do
+ 	@opportunity_unitTest = Opportunity.new :code => '001',
+ 	:company => 'Company 001',
+ 	:address => 'Address 001',
+ 	:salary => '1000',
+ 	:start_date => '04-08-2013', 
+ 	:end_date	=> '04-08-2014',
+ 	:requirements => 'Computers'
+ 	assert @opportunity_unitTest.save
+ 	first_company = Opportunity.find(@opportunity_unitTest.id).company
+ 	@opportunity_unitTest.company = 'Company XYZ'
+ 	assert @opportunity_unitTest.save
+ 	assert_not_equal Opportunity.find(@opportunity_unitTest.id).company, first_company
+ end
+
+ test "opportunity is deleted successfully" do
+ 	@opportunity_unitTest = Opportunity.new :code => '001',
+ 	:company => 'Company 001',
+ 	:address => 'Address 001',
+ 	:salary => '1000',
+ 	:start_date => '04-08-2013', 
+ 	:end_date	=> '04-08-2014',
+ 	:requirements => 'Computers'
+ 	assert @opportunity_unitTest.save
+ 	assert Opportunity.find(@opportunity_unitTest.id).destroy
+ end
 end
