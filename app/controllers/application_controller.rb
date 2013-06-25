12911c57e2d0e1911a6d7b42236cfb57f42caac9
Rodrigo Tolledo <rodrigotolledo@gmail.com>
2013-06-24 23:59:16 -0300

    adding profile capabilities for student, company and director.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,32 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  helper_method :current_user
+  helper_method :current_user, :student?, :company?, :director?
 
   private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def student?
+  	@who_is = User.find(session[:user_id]).email
+  	if @who_is == "student@student.com"
+  		true
+  	end
+  end
+
+  def company?
+  	@who_is = User.find(session[:user_id]).email
+  	if @who_is == "company@company.com"
+  		true
+  	end
+  end
+
+  def director?
+  	@who_is = User.find(session[:user_id]).email
+  	if @who_is == "director@director.com"
+  		true
+  	end
   end
 end
