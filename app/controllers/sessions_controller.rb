aa3b52b4cf3553c50358830475266f773c46a125
Rodrigo Tolledo <rodrigotolledo@gmail.com>
2013-07-01 16:27:03 -0300

    minor changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:email], params[:password])
     if user
       session[:user_id] = user.id
-      #redirect_to root_url, :notice => "Logged in!"
       redirect_to opportunities_path
     else
       flash.now.alert = "Invalid email or password"
